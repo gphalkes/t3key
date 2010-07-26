29e11df43b1f18cf40480c4400a96ff04426d6a5
Gertjan Halkes <hg@ghalkes.nl>
2010-07-26 20:30:09 +0200

    Added copyright notices to scripts and Makefile

--- src/extract_shared.sh
+++ src/extract_shared.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# Copyright (C) 2010 G.P. Halkes
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, as
+# published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # This script extracts the common keys from a key map and creates a new map
 # which uses a shared map for the common keys
