a6c5c703b2980fe39baf63175caeb35247f808e3
Gertjan Halkes <hg@ghalkes.nl>
2012-01-04 17:45:47 +0100

    t3keyc mostly finished

--- src.util/t3keyc/t3keyc.c
+++ src.util/t3keyc/t3keyc.c
@@ -13,10 +13,12 @@
 */
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
 
 #include <curses.h>
 #include <term.h>
+#include <ctype.h>
 
 #include <t3config/config.h>
 
@@ -33,6 +35,8 @@ static const char map_schema[] = {
 
 static bool option_link;
 static bool option_trace_circular;
+static bool option_check_terminfo = true;
+static bool option_verbose;
 static const char *input;
 
 #include "mappings.c"
@@ -73,7 +77,8 @@ static void print_usage(void) {
 	printf("Usage: t3keyc [<OPTIONS>] <INPUT>\n"
 		"  -h, --help                       Print this help message\n"
 		"  -l, --link                       Create symbolic links for aliases\n"
-		"  -t, --trace-circular-use         Trace circular 'use' inclusion\n");
+		"  -t, --trace-circular-use         Trace circular 'use' inclusion\n"
+		"  -v, --verbose                    Verbose output\n");
 	exit(EXIT_SUCCESS);
 }
 
@@ -113,6 +118,9 @@ static PARSE_FUNCTION(parse_options)
 		END_OPTION
 		OPTION('t', "trace-circular-use", NO_ARG)
 			option_trace_circular = true;
+		END_OPTION
+		OPTION('v', "verbose", NO_ARG)
+			option_verbose = true;
 		END_OPTION
 		DOUBLE_DASH
 			NO_MORE_OPTIONS;
@@ -244,6 +252,34 @@ static size_t parse_escapes(char *string) {
 	return write_position;
 }
 
+/* Convert a sequence to a printable representation. */
+static char *get_print_seq(const char *seq) {
+	static char buffer[1024];
+	char *dest = buffer;
+
+	while (*seq && dest - buffer < 1018) {
+		if (*seq == '"') {
+			*dest++ = '\\';
+			*dest++ = '"';
+			seq++;
+		} else if (*seq == '\\') {
+			*dest++ = *seq;
+			*dest++ = *seq++;
+		} else if (isprint(*seq)) {
+			*dest++ = *seq++;
+		} else if (*seq == 27) {
+			*dest++ = '\\';
+			*dest++ = 'e';
+			seq++;
+		} else {
+			sprintf(dest, "\\%03o", *seq++);
+			dest += 4;
+		}
+	}
+	*dest = 0;
+	return buffer;
+}
+
 typedef struct map_list_t {
 	t3_config_t *map;
 	struct map_list_t *next;
@@ -273,6 +309,7 @@ static void add_sequence(t3_config_t *ptr, t3_config_t *noticheck) {
 	size_t i;
 	char *str = safe_strdup(t3_config_get_string(ptr));
 	size_t str_len = parse_escapes(str);
+	bool invalid_name = false;
 
 	name = t3_config_get_name(ptr);
 	minus = strchr(name, '-');
@@ -281,8 +318,26 @@ static void add_sequence(t3_config_t *ptr, t3_config_t *noticheck) {
 		if (strncmp(name, valid_names[i], name_len) == 0 && name_len == strlen(valid_names[i]))
 			break;
 	}
-	if (i != ARRAY_LENGTH(valid_names)) {
-		//FIXME: check function keys
+
+	if (i == ARRAY_LENGTH(valid_names)) {
+		if (!(name[0] == 'f' && is_asciidigit(name[1]) &&
+				(name[2] == 0 || name[2] == '-' || (is_asciidigit(name[2]) &&
+				(name[3] == 0 || name[3] == '-') && name[1] != '0'))))
+		{
+		//FIXME: get file name information
+			fprintf(stderr, "%s:%d: '%s' is not a valid key name\n",
+				input, t3_config_get_line_number(ptr), t3_config_get_name(ptr));
+			invalid_name = true;
+		}
+	}
+
+	if (minus != NULL && strcmp(minus + 1, "s") != 0 && strcmp(minus + 1, "m") != 0 &&
+			strcmp(minus + 1, "c") != 0 && strcmp(minus + 1, "cm") != 0 && strcmp(minus + 1, "cs") != 0 &&
+			strcmp(minus + 1, "ms") != 0 && strcmp(minus + 1, "cms") != 0)
+	{
+		fprintf(stderr, "%s:%d: '%s' includes incorrect modifier sequence\n",
+				input, t3_config_get_line_number(ptr), t3_config_get_name(ptr));
+		invalid_name = true;
 	}
 
 	for (seq_ptr = seq_head; seq_ptr != NULL; seq_ptr = seq_ptr->next) {
@@ -303,16 +358,35 @@ static void add_sequence(t3_config_t *ptr, t3_config_t *noticheck) {
 		seq_ptr->next = seq_head;
 		seq_head = seq_ptr;
 	}
-	if (t3_config_find(noticheck, compare_name, name, NULL) == NULL) {
+	if (option_check_terminfo && !invalid_name && t3_config_find(noticheck, compare_name, name, NULL) == NULL) {
+		const char *tistr;
 		for (i = 0; i < ARRAY_LENGTH(keymapping); i++) {
 			if (strcmp(name, keymapping[i].key) == 0) {
-				//FIXME: check ti string
+				tistr = tigetstr(keymapping[i].tikey);
 				break;
+				return;
 			}
 		}
-		if (i != ARRAY_LENGTH(keymapping))
-			return;
-		//FIXME: check function keys (take shiftfn into account)
+		if (i == ARRAY_LENGTH(keymapping)) {
+			char buffer[100];
+			if (!(name[0] == 'f' && is_asciidigit(name[1])))
+				return;
+			if (minus != NULL)
+				return;
+			buffer[0] = 'k';
+			strcpy(buffer + 1, name);
+			tistr = tigetstr(buffer);
+		}
+
+		if (tistr != (char *) 0 && tistr != (char *) -1) {
+			if (strlen(tistr) != str_len || memcmp(tistr, str, str_len) != 0) {
+		//FIXME: get file name information
+				fprintf(stderr, "%s:%d: '%s' has different definition (%s) than terminfo (%s)\n",
+					input, t3_config_get_line_number(ptr), t3_config_get_name(ptr),
+					t3_config_get_string(ptr), get_print_seq(tistr));
+			}
+		}
+
 	}
 }
 
@@ -395,6 +469,31 @@ static void check_maps(t3_config_t *map_config) {
 	for (map = t3_config_get(t3_config_get(map_config, "maps"), NULL); map != NULL; map = t3_config_get_next(map)) {
 		if (t3_config_get_name(map)[0] != '_')
 			check_map_rec(map_config, map);
+		while (seq_head != NULL) {
+			sequence_list_t *tmp = seq_head;
+			seq_head = tmp->next;
+			free(tmp->str);
+			free(tmp);
+		}
+	}
+}
+
+static void create_symlinks(t3_config_t *map_config, const char *term_name) {
+	const t3_config_t *aka;
+	char *dirname = safe_strdup(input);
+	char *linkname;
+
+	if (strrchr(dirname, '/') != NULL)
+		strrchr(dirname, '/')[1] = 0;
+
+	for (aka = t3_config_get(t3_config_get(map_config, "aka"), NULL); aka != NULL; aka = t3_config_get_next(aka)) {
+		linkname = safe_malloc(strlen(dirname) + strlen(t3_config_get_string(aka)) + 1);
+		strcpy(linkname, dirname);
+		strcat(linkname, t3_config_get_string(aka));
+		if (symlink(term_name, linkname) == -1) {
+			if (errno != EEXIST || option_verbose)
+				fprintf(stderr, "Could not create symbolic link %s -> %s: %s\n", linkname, term_name, strerror(errno));
+		}
 	}
 }
 
@@ -403,6 +502,8 @@ int main(int argc, char *argv[]) {
 	t3_config_opts_t opts;
 	t3_config_error_t error;
 	t3_config_schema_t *schema;
+	const char *term_name;
+	int err;
 	FILE *file;
 
 	parse_options(argc, argv);
@@ -422,18 +523,23 @@ int main(int argc, char *argv[]) {
 	if (!t3_config_validate(map_config, schema, &error, T3_CONFIG_VERBOSE_ERROR))
 		fatal("%s:%d: %s%s%s\n", input, error.line_number, t3_config_strerror(error.error),
 			error.extra == NULL ? "" : ": ", error.extra == NULL ? "" : error.extra);
+	t3_config_delete_schema(schema);
+
+	if ((term_name = strrchr(input, '/')) == NULL)
+		term_name = input;
+	else
+		term_name++;
 
 	if (option_link) {
-		printf("NOT IMPLEMENTED YET\n");
-		exit(1);
+		create_symlinks(map_config, term_name);
+		exit(EXIT_SUCCESS);
+	}
+
+	if (setupterm(term_name, 1, &err) == ERR) {
+		fprintf(stderr, "Could not find terminfo for %s\n", term_name);
+		option_check_terminfo = false;
 	}
 	check_maps(map_config);
-
-	/* FIXME: checks to implement:
-		- map 'use's are not circular (although that should only trigger a warning)
-		- each sequence only occurs once for each top-level map
-		- definitions follow terminfo, unless the key is specifically named in noticheck
-		- shiftfn values are sensible
-	*/
-	return 0;
+	t3_config_delete(map_config);
+	return EXIT_SUCCESS;
 }
