def383ca9913190275a272a8fca827467ac25e2b
Gertjan Halkes <hg@ghalkes.nl>
2012-01-05 21:11:06 +0100

    Release files updates

--- dist_config.sh
+++ dist_config.sh
@@ -1,6 +1,5 @@
 PACKAGE=libt3key
 SRCDIRS="src src.util"
 EXCLUDESRC="/(Makefile|TODO.*|SciTE.*|run\.sh|test\.c)$"
-GENSOURCES="`echo src.util/t3keyc/.objects/*.{c,h}` src/key_api.h src/key_errors.h src/key_shared.c"
-AUXSOURCES="src.util/t3keyc/.objects/mappings.c"
+GENSOURCES="`echo src.util/t3keyc/.objects/*.c` src/.objects/map.bytes src/key_api.h src/key_errors.h src/key_shared.c"
 VERSIONINFO="1:0:0"
--- mkdist.sh
+++ mkdist.sh
@@ -15,7 +15,7 @@ get_sources_hg
 make_tmpdir
 copy_sources ${SOURCES} ${GENSOURCES} ${AUXSOURCES}
 copy_dist_files
-copy_files doc/API doc/binary_format.txt doc/text_format.txt `hg manifest | egrep '^man/'`
+copy_files doc/API doc/format.txt `hg manifest | egrep '^man/'`
 create_configure
 
 if [[ "${VERSION}" =~ [0-9]{8} ]] ; then
@@ -30,7 +30,7 @@ sed -i "/#define T3_KEY_VERSION/c #define T3_KEY_VERSION ${VERSION_BIN}" ${TOPDI
 ( cd ${TOPDIR}/src ; ln -s . t3key )
 
 OBJECTS_LIBT3KEY="`echo \"${SOURCES} ${GENSOURCES} ${AUXSOURCES}\" | tr ' ' '\n' | sed -r 's%\.objects/%%' | egrep '^src/[^/]*\.c$' | sed -r 's/\.c\>/.lo/g' | tr '\n' ' '`"
-OBJECTS_T3KEYC="`echo \"${SOURCES} ${GENSOURCES}\" | tr ' ' '\n' | sed -r 's%\.objects/%%' | egrep '^src\.util/t3keyc/.*\.c$' | sed -r 's/\.c\>/.o/g' | tr '\n' ' '`"
+OBJECTS_T3KEYC="`echo \"${SOURCES} ${GENSOURCES} ${AUXSOURCES}\" | tr ' ' '\n' | sed -r 's%\.objects/%%' | egrep '^src\.util/t3keyc/.*\.c$' | sed -r 's/\.c\>/.o/g' | tr '\n' ' '`"
 OBJECTS_T3LEARNKEYS="`echo \"${SOURCES} ${GENSOURCES} ${AUXSOURCES}\" | tr ' ' '\n' | sed -r 's%\.objects/%%' | egrep '^src\.util/t3learnkeys/.*\.c$' | sed -r 's/\.c\>/.o/g' | tr '\n' ' '`"
 
 #FIXME: somehow verify binary compatibility, and print an error if not compatible
