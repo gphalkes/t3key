8606fc2e43836c94e6e4ba33050bd6adcfed6e48
Gertjan Halkes <hg@ghalkes.nl>
2012-11-19 20:01:15 +0100

    Fix omission in version 0.2.2

--- mkdist.sh
+++ mkdist.sh
@@ -15,7 +15,7 @@ get_sources_hg
 make_tmpdir
 copy_sources ${SOURCES} ${GENSOURCES} ${AUXSOURCES}
 copy_dist_files
-copy_files doc/API doc/format.txt
+copy_files doc/API doc/format.txt doc/supplemental.kmap
 create_configure
 
 if [[ "${VERSION}" =~ [0-9]{8} ]] ; then
